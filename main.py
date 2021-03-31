6e3a17590aa0a19c8b5b07ec816f6892af3547a3
Jakub Kučera <kucerj56@fit.cvut.cz>
2021-03-31 22:59:46 +0200

    Added data storage explanation

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("hello, friend")
+from rtree import rtree, database_entry
+# from
+
+
+if __name__ == '__main__':
+    print("hello, friend")
+
+
+"""
+
+leaf_flag, N * [min, max], K * id
+leaf_flag, N * [min, max], K * byte address
+
+flag, N * [min, max], data
+
+K * byte address
+
+========================
+"""
