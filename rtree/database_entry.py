e0d8d8a22f26c8b09310c86864e8748d5ac87b55
Matěj Jezdinský <matejjezdinsky@gmail.com>
2021-04-25 13:58:03 +0200

    Rename delete tag

--- rtree/database_entry.py
+++ rtree/database_entry.py
@@ -3,6 +3,6 @@ from typing import List
 
 class DatabaseEntry:
     def __init__(self, coordinates: List[int], data: object = None):
-        self.to_be_removed = False
+        self.is_present = False
         self.coordinates = coordinates
         self.data = data # can be empty
