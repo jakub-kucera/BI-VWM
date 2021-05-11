62ccc9e6c459adf03d229e25c13a4c9bdacf08f2
Jakub Kučera <kucerj56@fit.cvut.cz>
2021-05-11 12:52:44 +0200

    Put MBBDim into separate class

--- rtree/mbb.py
+++ rtree/mbb.py
@@ -1,34 +1,8 @@
-"""Includes class for MMB and its helper class MMBDim which represents one dimension"""
 from __future__ import annotations
 
 from typing import Tuple, List
 
-
-class MBBDim:
-    """Class represents a single dimension in a MMB."""
-
-    def __init__(self, low: int, high: int):
-        if low < high:
-            self.low = low
-            self.high = high
-        else:
-            self.low = high
-            self.high = low
-
-    def __eq__(self, other):
-        return self.__dict__ == other.__dict__
-
-    def get_diff(self):
-        return self.high - self.low
-
-    def contains(self, inner: MBBDim):
-        return inner.low >= self.low and inner.high <= self.high
-
-    def overlaps(self, other: MBBDim):
-        return (other.low <= self.low <= other.high) \
-               or (other.low <= self.high <= other.high) \
-               or (self.low <= other.low <= self.high) \
-               or (self.low <= other.high <= self.high)
+from rtree.mbb_dim import MBBDim
 
 
 class MBB:
