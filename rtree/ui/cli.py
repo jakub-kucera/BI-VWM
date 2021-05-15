11ddaa493cb9b34532f1b6ca464f64f40b05aad3
Matěj Jezdinský <matejjezdinsky@gmail.com>
2021-05-15 03:09:28 +0200

    CLI insert entry and search for point

--- rtree/ui/cli.py
+++ rtree/ui/cli.py
@@ -1,50 +1,25 @@
 # from rtree.rtree import RTree
 # from rtree.default_config import *
 
+import os
+
+from typing import List
+from rtree.rtree import RTree
+from rtree.data.database_entry import DatabaseEntry
+from rtree.ui.cli_support import *
+
 
 class CLI:
     def __init__(self):
         self.file_name = ""
-        # self.rtree = None
+        self.tree = None
         self.dimensions = 2
 
         self.__init_setup()
 
-    def __input(self):
-        tmp = input(">> ")
-        for x in ["exit", "e", "quit", "q"]:
-            if x == tmp.lower():
-                exit()
-        for x in ["help", "h", "?"]:
-            if x == tmp.lower():
-                self.__print_help()
-                return self.__input()
-        return tmp
-
-    def __matches(self, text, options) -> bool:
-        for x in options:
-            if x == text:
-                return True
-        return False
-
-    def __print_help(self):
-        print("+==========================================================+\n"
-              "| CLI:\n"
-              "| - For every choice, you can write the whole text, number\n"
-              "| or just the capitals, for example '1> Exit' will accept\n"
-              "| 'e' or '1' as an input\n"
-              "| - To exit the application using 'exit', 'e', 'quit' or 'q'\n"
-              "| - Get help using 'help', 'h' or '?'\n"
-              "| R-Tree:\n"
-              "| - Working binary files are stored in './saved_data' folder\n"
-              "| - Filenames cannot contain spaces or start with a number,\n"
-              "| other OS specific exceptions also apply\n"
-              "+==========================================================+")
-        return
-
     def __init_setup(self):
         print("+==========================================================+\n"
-              "|                  R-Tree CLI  beta-0.1                    |\n"
+              "|                  R-Tree CLI  beta-0.3                    |\n"
               "| FIT CVUT 2021                                            |\n"
               "| BI-VWM                                kucerj56, jezdimat |\n"
               "+==========================================================+\n"
@@ -57,94 +32,155 @@ class CLI:
         self.__default_menu()
 
     def __setup_database_file(self):
-        db_choice = self.__input()
-        if self.__matches(db_choice, ["c", "1"]):
+        db_choice = get_input()
+        if matches(db_choice, ["c", "1"]):
 
             print("Set name for new database")
-            self.file_name = self.__input()
+            while True:
+                self.file_name = get_input()
+                if is_valid_db_name(self.file_name):  # exists?
+                    break
+                else:
+                    print("Try again")
+
             print(f"Created database file 'saved_data/{self.file_name}.bin'\n"
                   f"Created r-tree file 'saved_data/{self.file_name}.rtree.bin'")
 
-            self.__setup_dimensions()
-            '''self.rtree = RTree(tree_file=self.file_name + ".rtree.bin",
+            print("How many dimensions should r-tree use? (1 - 16)")
+            while True:
+                try:
+                    self.dimensions = int(get_input())
+                except TypeError:
+                    print("Try again")
+
+                if 1 <= self.dimensions <= 16:
+                    break
+                else:
+                    print("Try again")
+
+            print(f"R-tree has {self.dimensions} 64-bit Int dimensions")
+
+            self.tree = RTree()
+            '''self.tree = RTree(tree_file=self.file_name + ".rtree.bin",
                                database_file=self.file_name + ".bin",
                                dimensions=self.dimensions,
                                parameters_size=8)'''
 
-        elif self.__matches(db_choice, ["l", "2"]):
+        elif matches(db_choice, ["l", "2"]):
 
-            print("Insert database file from saved_data/[name]")
-            self.file_name = self.__input()
+            print("Select database file from saved_data/[name]")  # print existing databases
+
+            while True:
+                self.file_name = get_input()
+                if is_valid_db_name(self.file_name):  # exists?
+                    break
+                else:
+                    print("Try again")
+
             print(f"Loaded database file 'saved_data/{self.file_name}.bin'\n"
                   f"Loaded r-tree file 'saved_data/{self.file_name}.rtree.bin'")
 
-            '''self.rtree = RTree(tree_file=self.file_name + ".rtree.bin",
+            '''self.tree = RTree(tree_file=self.file_name + ".rtree.bin",
                                database_file=self.file_name + ".bin")'''
 
         else:
             print("Unrecognized input, try again!")
             self.__setup_database_file()
 
-    def __setup_dimensions(self):
-        print("How many dimensions should r-tree use?")
-        self.__setup_dimensions_loop()
-        print(f"R-tree has {self.dimensions} 64-bit Int dimensions")
-
-    def __setup_dimensions_loop(self):
-        try:
-            self.dimensions = int(self.__input())
-        except ValueError:
-            print("That's not an int!")
-            self.__setup_dimensions_loop()
-        return
-
     def __default_menu(self):
-        print("============================================================\n"
-              f"files: {self.file_name}, dimensions: {self.dimensions}\n"
+        print("\n============================================================\n"
+              f"files: {self.file_name}, dimensions: {self.dimensions}, entries: NaN\n"
               "1> Add to database\n"
-              "2> Remove from database\n"
+              "TODO Remove from database\n"
               "3> Search for Point\n"
-              "4> Search for Range\n"
-              "5> Search for Nearest neighbour\n"
-              "6> Print graph")
+              "TODO Search for Range\n"
+              "TODO Search for Nearest neighbour\n"
+              "TODO Print graph\n"
+              "TODO Delete Tree and Rebuild\n"
+              "TODO Delete Tree and Database")
         self.__default_loop()
 
     def __default_loop(self):
-        action = self.__input()
-        if self.__matches(action, ["a", "1"]):
+        action = get_input()
+        if matches(action, ["a", "1"]):
 
-            print("Set dimension 1, 2, 3, .., n - Set content")
-            # self.__add_entry()
+            self.__add_entry()
 
-        elif self.__matches(action, ["r", "2"]):
+        elif matches(action, ["r", "2"]):
 
             print("Set dimension 1, 2, 3, .., n - or node_id")
             # self.__remove_entry()
 
-        elif self.__matches(action, ["sp", "3"]):
+        elif matches(action, ["sp", "3"]):
 
-            print("Set dimension 1, 2, 3, .., n")
-            # self.__search_point()
+            self.__search_point()
 
-        elif self.__matches(action, ["sr", "4"]):
+        elif matches(action, ["sr", "4"]):
 
             print("Set dimension rage 1, 2, 3, .., n * 2")
             # self.__search_range()
 
-        elif self.__matches(action, ["sn", "5"]):
+        elif matches(action, ["sn", "5"]):
 
             print("Set k - Set dimensions range 1, 2, 3, .., n * 2")
             # self.__search_knn()
 
-        elif self.__matches(action, ["p", "6"]):
+        elif matches(action, ["p", "6"]):
 
             print("Display visual")
+            # self.__graph()
+
+        elif matches(action, ["dtr", "13"]):
+
+            print("Delete Tree and Rebuild")
+            # self.__graph()
+
+        elif matches(action, ["dtd", "19"]):
+
+            print("Delete Tree and Database")
             # self.__graph()
 
         else:
             print("Unrecognized input, try again!")
 
-        self.__default_loop()
+        self.__default_menu()
+
+    def __search_point(self):
+        coord: List[int] = []
+        for n in range(self.dimensions):
+            print("Set dimension", n + 1)
+            while True:
+                try:
+                    tmp = int(get_input())
+                    coord.append(tmp)
+                    break
+                except TypeError:
+                    print("Try again")
+
+        entry = self.tree.search_node(coord)
+        if entry is None:
+            print("No entry found on given coordinates")
+        else:
+            print(entry.data)
+
+    def __add_entry(self):
+        coord: List[int] = []
+        for n in range(self.dimensions):
+            print("Set dimension", n + 1)
+            while True:
+                try:
+                    tmp = int(get_input())
+                    coord.append(tmp)
+                    break
+                except TypeError:
+                    print("Try again")
+
+        print("Insert data for this entry:")
+        data = input()
+
+        entry = DatabaseEntry(coord, data)
+        self.tree.insert_entry(entry)
+        print(f"Entry '{entry.data}' saved, at position '{coord}'")
 
 
 if __name__ == "__main__":
---
+++ rtree/ui/cli_support.py
@@ -0,0 +1,47 @@
+
+def get_input():
+    tmp = input(">> ")
+    for x in ["exit", "e", "quit", "q"]:
+        if x == tmp.lower():
+            exit()
+    for x in ["help", "h", "?"]:
+        if x == tmp.lower():
+            print_help()
+            return get_input()
+    return tmp
+
+
+def matches(text, options) -> bool:
+    for x in options:
+        if x == text:
+            return True
+    return False
+
+
+def is_num(value) -> bool:
+    try:
+        value = int(value)
+    except TypeError:
+        return False
+    return True
+
+
+def is_valid_db_name(value) -> bool:
+    if type(value[0]) == int or len(value) <= 3:
+        return False
+    return type(value) == str
+
+
+def print_help():
+    print("+==========================================================+\n"
+          "| CLI:\n"
+          "| - For every choice, you can write the whole text, number\n"
+          "| or just the capitals, for example '1> Exit' will accept\n"
+          "| 'e' or '1' as an input\n"
+          "| - To exit the application using 'exit', 'e', 'quit' or 'q'\n"
+          "| - Get help using 'help', 'h' or '?'\n"
+          "| R-Tree:\n"
+          "| - Working binary files are stored in './saved_data' folder\n"
+          "| - Filenames cannot contain spaces or start with a number,\n"
+          "| other OS specific exceptions also apply\n"
+          "+==========================================================+")
